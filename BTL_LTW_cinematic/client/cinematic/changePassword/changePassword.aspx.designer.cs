a1a994d4b68f88969a5e24120615c67900fb32a5
phinguyen120 <phinguyendinh120@gmail.com>
2024-07-24 00:01:19 +0700

    fix xong đổi mật khẩu

--- BTL_LTW_cinematic/client/cinematic/changePassword/changePassword.aspx.designer.cs
+++ BTL_LTW_cinematic/client/cinematic/changePassword/changePassword.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace BTL_LTW_cinematic.client.cinematic.changePassword
     {
 
         /// <summary>
-        /// changePasswordForm control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm changePasswordForm;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// submit control.
